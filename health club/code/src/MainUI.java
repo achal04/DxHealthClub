099c2ca70841d6b9ff0066a1bda466a3d858d1d3
Achal Agarwal <achal.agarwal@live.com>
2013-11-16 00:16:05 +0530

    Health Club  V1.0.0

---
+++ health club/code/src/MainUI.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Agarwal's
+ */
+class MainUI {
+
+    static void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+}
---
+++ health club/code/src/healthclub/MainUI.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package healthclub;
+
+/**
+ *
+ * @author Agarwal's
+ */
+class MainUI {
+
+    static void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+}
